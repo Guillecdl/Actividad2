db187ddba419d2cf03f61acd24b4aaad351b9160
Guillecdl <58507105+Guillecdl@users.noreply.github.com>
2020-01-10 13:07:36 +0000

    Update db_manager.php

--- userscrud-master/util/db_manager.php
+++ userscrud-master/util/db_manager.php
@@ -66,6 +66,9 @@
 
     function delete_user ($user_id){
         // COMPLETAR ELIMINAR DE USUARIO
+        $sql = "DELETE FROM user WHERE id='".$user_id."'";
+        $conn = $GLOBALS['conn'];
+        return $conn->query($sql) ;
 
     }
 
